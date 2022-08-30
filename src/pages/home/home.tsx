a1e60e7178a3fff3e3525d54cfb2cca7cceb320e
PurpleCool <mariagruzintseva7@gmail.com>
2022-08-30 08:57:10 +0300

    initial preparations

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+      </header>
+    </div>
+  );
+}
+
+export default Home;
